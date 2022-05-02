5f00f6d92d179a6ac5ccbcd3db62ff0a07573411
dedku <szabat.patryk@gmail.com>
2022-05-02 23:38:40 +0200

    cloudinary settings change

--- schema/newAssets.ts
+++ schema/newAssets.ts
@@ -7,9 +7,9 @@ export const isAdmin = ({session}:{session:SessionContext}) => session?.data.isA
 // Cloud image setup
 export const cloundImage = cloudinaryImage({
     cloudinary: {
-      cloudName: process.env.CLOUDINARY_CLOUD_NAME! || 'drx4dy0ep',
-      apiKey: process.env.CLOUDINARY_API_KEY! || '851415739516514',
-      apiSecret: process.env.CLOUDINARY_API_SECRET! || 'B8_GOCevaxPbH1HFyiWhsgpN-6s',
-      folder: process.env.CLOUDINARY_API_FOLDER! || 'home',
+      cloudName: process.env.CLOUDINARY_CLOUD_NAME!,
+      apiKey: process.env.CLOUDINARY_API_KEY!,
+      apiSecret: process.env.CLOUDINARY_API_SECRET!,
+      folder: process.env.CLOUDINARY_API_FOLDER!
     },
   })
