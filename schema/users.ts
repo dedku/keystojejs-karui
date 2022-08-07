5060e8a0710072815b59b27aaa2fb269213bdf16
Patryk Szabat <szabat.patryk@gmail.com>
2022-08-07 22:23:26 +0200

    isOnline setup

--- schema/users.ts
+++ schema/users.ts
@@ -1,75 +1,84 @@
 import { list } from '@keystone-6/core';
 import {
-    text,
-    relationship,
-    password,
-    checkbox,
-  } from '@keystone-6/core/fields';;
+  text,
+  relationship,
+  password,
+  checkbox,
+} from '@keystone-6/core/fields';;
 import { filterUser, isAdmin, showIfAdmin, cloundImage } from './newAssets';
 
 
-export const User =  list({
-    access: {
-        operation: {
-            delete: isAdmin,
-        },
-        filter: {
-          query: filterUser,
-        }
+export const User = list({
+  access: {
+    operation: {
+      delete: isAdmin,
     },
-    fields: {
-      name: text({
-        validation: { isRequired: true },
-      }),
-      email: text({
-        validation: { isRequired: true },
-        isIndexed: 'unique',
-        isFilterable: true,
-      }),
-      slug: text({
-        access:{
-          update: isAdmin,
-        }
-      }),
-      image: cloundImage,
-      isAdmin: checkbox({
-        defaultValue: false,
-        access: {
-            update: isAdmin,
-        },
-        ui:{
-           createView: { fieldMode: showIfAdmin }, itemView: { fieldMode: showIfAdmin }
-        }
-      }),
-      password: password({ validation: { isRequired: true } }),
-      blogs: relationship({
-        ref: 'Blog.author',
-        many: true,
-        ui: {
-          hideCreate: !isAdmin,
-          createView: { fieldMode: 'hidden' },
-        },
-      }),
-      builds: relationship({
-        ref: 'Build.author',
-        many: true,
-        ui: {
-          hideCreate: !isAdmin,
-          createView: { fieldMode: 'hidden' },
-        },
-      }),
-    },
-    hooks:{
-      // Create slug
-      resolveInput: ({ resolvedData }) => {
-        const { name } = resolvedData;
-        if (name) {
-          return {
-            ...resolvedData,
-            slug: name?.trim()?.toLowerCase()?.replace(/[^\w ]+/g, '')?.replace(/ +/g, '-') ?? ''
-          }
-        }
-        return resolvedData;
-      }
+    filter: {
+      query: filterUser,
     }
-  })
+  },
+  fields: {
+    name: text({
+      validation: { isRequired: true },
+    }),
+    email: text({
+      validation: { isRequired: true },
+      isIndexed: 'unique',
+      isFilterable: true,
+    }),
+    slug: text({
+      access: {
+        update: isAdmin,
+      }
+    }),
+    image: cloundImage,
+    isAdmin: checkbox({
+      defaultValue: false,
+      access: {
+        update: isAdmin,
+      },
+      ui: {
+        createView: { fieldMode: showIfAdmin }, itemView: { fieldMode: showIfAdmin }
+      }
+    }),
+    isOnline: checkbox({
+      defaultValue: false,
+      access: {
+        update: isAdmin,
+      },
+      ui: {
+        createView: { fieldMode: showIfAdmin }, itemView: { fieldMode: showIfAdmin }
+      }
+    }),
+    password: password({ validation: { isRequired: true } }),
+    blogs: relationship({
+      ref: 'Blog.author',
+      many: true,
+      ui: {
+        hideCreate: !isAdmin,
+        createView: { fieldMode: 'hidden' },
+      },
+    }),
+    builds: relationship({
+      ref: 'Build.author',
+      many: true,
+      ui: {
+        hideCreate: !isAdmin,
+        createView: { fieldMode: 'hidden' },
+      },
+    }),
+  },
+  hooks: {
+    // Create slug
+    resolveInput: ({ resolvedData }) => {
+      const { name } = resolvedData;
+      if (name) {
+        return {
+          ...resolvedData,
+          slug: name?.trim()?.toLowerCase()?.replace(/[^\w ]+/g, '')?.replace(/ +/g, '-') ?? ''
+        }
+      }
+      return resolvedData;
+    }
+  }
+})
