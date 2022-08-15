479efdd7d0bd121deb132e1ea5f581d1671e8347
Patryk Szabat <szabat.patryk@gmail.com>
2022-08-15 15:55:34 +0200

    update

--- schema/users.ts
+++ schema/users.ts
@@ -7,7 +7,6 @@ import {
 } from '@keystone-6/core/fields';;
 import { filterUser, isAdmin, showIfAdmin, cloundImage } from './newAssets';
 
-//test
 export const User = list({
   access: {
     operation: {
